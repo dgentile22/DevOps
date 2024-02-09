990ac736a8ee4c980f4f231415f82a03775f1022
dgentile22 <gentiled@patrickind.com>
2024-02-09 14:17:08 -0500

    test 2

--- WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp2/Form1.Designer.cs
@@ -33,7 +33,7 @@
          // 
          // textBox1
          // 
-         this.textBox1.Location = new System.Drawing.Point(328, 83);
+         this.textBox1.Location = new System.Drawing.Point(44, 378);
          this.textBox1.Name = "textBox1";
          this.textBox1.Size = new System.Drawing.Size(289, 23);
          this.textBox1.TabIndex = 0;
